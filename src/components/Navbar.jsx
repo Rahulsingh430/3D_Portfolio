74a05e206874c5862002f9b87e0917e92f386940
Rahulsingh430 <rahulsingh430@gmail.com>
2023-07-28 04:38:58 +0530

    nav updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,39 @@
-import React, {useState, useEffect} from 'react';
-import { Link } from 'react-router-dom';
-import {styles} from '../styles';
-import {navLinks} from '../constants';
-import {logo} from '../assets';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo2 } from "../assets";
 
 const Navbar = () => {
-  const [active, setActive]=useState(" ")
+  const [active, setActive] = useState(" ");
   return (
-    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
-<div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
-  <Link to='/' className='flex items-center gap-2' onClick={()=>{
-    setActive("");
-    window.scrollTo(0,0);
-  }}>
-    <img src={logo} alt="logo" className='w-9 h-9 object-contain'/>
-  </Link>
-</div>
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo2} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text=[18px] font-bold cursor-pointer">
+            Rahul Singh
+          </p>
+        </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li key={link.id} className={`${active === link.title? 'text-white':'text-secondary'} hover:text-white text-[18] fornt-medium cursor-pointer`} onClick={()=>setActive(link.title)}>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
