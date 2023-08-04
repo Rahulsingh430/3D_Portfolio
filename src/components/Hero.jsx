4d08104ad49805305f189f164eb660da777d02cb
Rahulsingh430 <rahulsingh430@gmail.com>
2023-08-05 03:11:36 +0530

    hero section is done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,10 +17,27 @@ const Hero = () => {
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi I'm <span className="text-[#f24841]">Rahul</span>
           </h1>
-          <p className={`${styles.heroSubText} text-white-100 mt-2 `}>I Create Amazing Pixel perfect Design using Javascript Libraries, <br className="sm:block hidden" /> I call myself Frontend Developer. Althoug i do know Backend and its integration as well.</p>
+          <p className={`${styles.heroSubText} text-white-100 mt-2 `}>
+            I Create Amazing Pixel perfect Design using Javascript Libraries,{" "}
+            <br className="sm:block hidden" /> I call myself Frontend Developer.
+          </p>
         </div>
       </div>
-      <ComputersCanvas/>
+      <ComputersCanvas />
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+       <a href="#about">
+        <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-center p-2 ">
+          <motion.dev
+            animate={{y:[0,24,0]}} 
+            transition={{
+              duration: 1.5,
+              repeat: Infinity,
+              repeatType: 'loop'
+            }}
+            className=" w-3 h-3 rounded-full bg-secondary mb-1"
+          />
+          </div></a>
+      </div>
     </section>
   );
 };
